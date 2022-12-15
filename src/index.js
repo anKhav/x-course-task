42601eec1942ed76485d61bc0b28c1ca9e6057e4
anKhav <khavaldzhi.a@gmail.com>
2022-12-15 17:56:30 +0100

    Deploy

--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    {/*<BrowserRouter>*/}
-      <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-      </HashRouter>
-    {/*</BrowserRouter>*/}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
